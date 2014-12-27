d8278a08f8c1b871055ea524bbb161d7c7620b77
thejinchao@gmail.com <thejinchao@gmail.com>
2014-12-28 00:15:41 +0800

    修改安卓工程缺省上传地址

--- axvoice.android/src/com/axia/Config.java
+++ axvoice.android/src/com/axia/Config.java
@@ -3,10 +3,8 @@ package com.axia;
 
 public class Config {
 	//Upload url
-	public static String uploadUrl = "http://111.206.74.78/upload_voice";
-	//public final static String uploadUrl = "http://10.1.8.123/upload_voice";
+	public static String uploadUrl = "http://dashengine.com/upload_voice";
 	//local cache file path 
 	public static String cacheAudioPath = UtilFunctions.getSDPath() + "cacheAudio/";
-	
-
 }
+
