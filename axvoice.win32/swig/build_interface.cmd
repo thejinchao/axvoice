348f84032bf98e92eda58fdd413a40026c643d74
Jin Chao <thejinchao@gmail.com>
2015-01-21 18:29:50 +0800

    去掉swig编译脚本中不在使用的语句

--- axvoice.win32/swig/build_interface.cmd
+++ axvoice.win32/swig/build_interface.cmd
@@ -8,6 +8,3 @@ swig -csharp -outcurrentdir -namespace com.axia -c++ ..\axvoice.i
 
 copy *.h ..
 copy *.cxx ..
-
-copy axvoicePINVOKE.cs D:\app_unity\voice_test\Assets\Plugins
-copy axvoice*Callback.cs D:\app_unity\voice_test\Assets\Plugins
